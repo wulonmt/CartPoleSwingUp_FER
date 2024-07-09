44dd801f807e08bf5e7b54473dfbdfa74a8492d2
wulonmt <zuck0087285@gmail.com>
2024-07-09 20:58:24 +0800

    add register max steps

--- Env/__init__.py
+++ Env/__init__.py
@@ -3,19 +3,23 @@ from gymnasium.envs.registration import register
 register(
     id='CartPoleSwingUp-v0',
     entry_point='Env.envs:CartPoleSwingUpV0',
+    max_episode_steps=500,
 )
 
 register(
     id='CartPoleSwingUpFixInitState-v0',
     entry_point='Env.envs:CartPoleSwingUpFixInitStateV0',
+    max_episode_steps=500,
 )
 
 register(
     id='CartPoleSwingUpFixInitState-v1',
     entry_point='Env.envs:CartPoleSwingUpFixInitStateV1',
+    max_episode_steps=500,
 )
 
 register(
     id='TorchCartPoleSwingUpFixInitState-v0',
     entry_point='Env.envs:TorchCartPoleSwingUpFixInitStateV0',
+    max_episode_steps=500,
 )
